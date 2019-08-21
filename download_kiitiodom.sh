6ac06d4f64bc3b0a0d28c772f716e49e95bbe4fe
song <602947547@qq.com>
2019-08-21 20:32:41 +1000

    download scripts for some SLAM datasets

---
+++ download_kiitiodom.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+
+wget "http://kitti.is.tue.mpg.de/kitti/data_odometry_calib.zip"
+wget "http://kitti.is.tue.mpg.de/kitti/data_odometry_poses.zip"
+wget "http://kitti.is.tue.mpg.de/kitti/data_odometry_color.zip"
+wget "http://kitti.is.tue.mpg.de/kitti/data_odometry_velodyne.zip"
+
+echo "uzip: calib"
+tar -xzf data_odometry_calib.zip
+echo "uzip: data_odometry_poses.zip"
+tar -xzf data_odometry_poses.zip
+echo "uzip: data_odometry_color.zip"
+tar -xzf data_odometry_color.zip
+echo "uzip: data_odometry_velodyne.zip"
+tar -xzf data_odometry_velodyne.zip
+
+mkdir zip
+mv *.zip zip
